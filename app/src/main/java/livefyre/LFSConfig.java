c595ba740cadc0f057bc7607585e4af8285a0712
Kvana <kvanadev1@gmail.com>
2015-07-27 21:15:11 +0530

    pile picker key removed

--- app/src/main/java/livefyre/LFSConfig.java
+++ app/src/main/java/livefyre/LFSConfig.java
@@ -5,7 +5,7 @@ public class LFSConfig {
 	public static String NETWORK_ID = "client-solutions.fyre.co";
 	public static String SITE_ID = "360354";
 	public static String ARTICLE_ID = "custom-1402701825509";
-	public static final String FILEPICKER_API_KEY = "AYNlO8P2PT6qnCfo9eCw2z";
+	public static final String FILEPICKER_API_KEY = "FILEPICKER_KEY";
 
 	public static String system = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkb21haW4iOiJjbGllbnQtc29sdXRpb25zLmZ5cmUuY28iLCJ1c2VyX2lkIjoic3lzdGVtIiwiZGlzcGxheV9uYW1lIjoic3lzdGVtIiwiZXhwaXJlcyI6MjkxNzY3MjY1MC42ODMsImlhdCI6MTQzNjUxNzQ1MH0.ttKySWgdTOxioCAiAFi_gF7hXR3y0dIBhtkORK0rctI";
 	public static String testUser = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkb21haW4iOiJjbGllbnQtc29sdXRpb25zLmZ5cmUuY28iLCJ1c2VyX2lkIjoidGVzdFVzZXIiLCJkaXNwbGF5X25hbWUiOiJ0ZXN0VXNlciIsImV4cGlyZXMiOjI5MTgwMjIxMDUuNDY3LCJpYXQiOjE0MzY4NjY5MDV9.G5h4O08uXocG4m9-Nw2E_EvdzpuIZPhfa1rJ3Ft6Toc";
